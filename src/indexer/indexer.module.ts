921880c3549f5d3f2d9fa113d3071bb538453fe0
Saurabh Gupta <saurabhgupta@mithyalabs.com>
2025-05-14 17:45:59 +0530

    Refactor IndexerModule and IndexerService for improved readability
    - Standardized import statements to use single quotes.
    - Reformatted provider configuration in IndexerModule for better alignment.
    - Simplified constructor in IndexerService for clarity.
    - Ensured consistent formatting and indentation throughout the files.

--- src/indexer/indexer.module.ts
+++ src/indexer/indexer.module.ts
@@ -4,18 +4,21 @@ import { Client } from '@elastic/elasticsearch';
 
 @Module({
 	imports: [],
-	providers: [IndexerService, {
-        provide: 'ESClient',
-        inject: [],
-        useFactory() {
-            return new Client({
-                nodes: ['http://es01:9200', 'http://es02:9201', 'http://es03:9202'],
-                auth: {
-                    username:'elastic',
-                    password: 'elastic_password',
-                },
-            });
-        },
-    }],
+	providers: [
+		IndexerService,
+		{
+			provide: 'ESClient',
+			inject: [],
+			useFactory() {
+				return new Client({
+					nodes: ['http://es01:9200', 'http://es02:9201', 'http://es03:9202'],
+					auth: {
+						username: 'elastic',
+						password: 'elastic_password',
+					},
+				});
+			},
+		},
+	],
 })
 export class IndexerModule {}
--- src/indexer/indexer.service.ts
+++ src/indexer/indexer.service.ts
@@ -1,22 +1,20 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable } from '@nestjs/common';
 import { Client } from '@elastic/elasticsearch';
 
 @Injectable()
 export class IndexerService {
+	constructor(@Inject('ESClient') private readonly esClient: Client) {
+		this.listAllIndices()
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 
-
-    constructor(
-		@Inject('ESClient') private readonly esClient: Client
-    ) {
-        this.listAllIndices().then((response) => {
-            console.log(response);
-        }).catch((error) => {
-            console.error(error);
-        });
-    }
-
-    async listAllIndices() {
-        const response = await this.esClient.cat.indices({ format: 'json' });
-        return response;
-    }
+	async listAllIndices() {
+		const response = await this.esClient.cat.indices({ format: 'json' });
+		return response;
+	}
 }
