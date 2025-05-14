f38fe588622a00aa2c68432c7d3c8f0aea21a43b
Saurabh Gupta <saurabhgupta@mithyalabs.com>
2025-05-14 19:44:45 +0530

    Enhance Docker and application configuration for MongoDB integration
    - Updated docker-compose.yml to expose debugging port and mount source code.
    - Modified package.json to allow remote debugging on port 9229.
    - Added MongoDB client configuration in IndexerModule for database connectivity.
    - Improved IndexerService to handle index creation and mapping updates with MongoDB integration.
    - Included additional TypeScript files in tsconfig.json for better type support.

--- src/indexer/indexer.module.ts
+++ src/indexer/indexer.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { IndexerService } from './indexer.service';
 import { Client } from '@elastic/elasticsearch';
+import { MongoClient } from 'mongodb';
 
 @Module({
 	imports: [],
@@ -11,12 +12,19 @@ import { Client } from '@elastic/elasticsearch';
 			inject: [],
 			useFactory() {
 				return new Client({
-					nodes: ['http://es01:9200', 'http://es02:9201', 'http://es03:9202'],
+					nodes: ['http://es01:9200', 'http://es02:9200', 'http://es03:9200'],
 					auth: {
 						username: 'elastic',
 						password: 'elastic_password',
 					},
 				});
+			},
+		},
+		{
+			provide: 'MongoClient',
+			inject: [],
+			useFactory() {
+				return new MongoClient('mongodb://localhost:27017/ltd_new');
 			},
 		},
 	],
--- src/indexer/indexer.service.ts
+++ src/indexer/indexer.service.ts
@@ -1,20 +1,57 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Client } from '@elastic/elasticsearch';
+import { MongoClient } from 'mongodb';
+import { IndicesCreateRequest, IndicesPutMappingRequest } from '@elastic/elasticsearch/lib/api/types';
+import * as fs from 'fs/promises';
+import path from 'path';
 
 @Injectable()
 export class IndexerService {
-	constructor(@Inject('ESClient') private readonly esClient: Client) {
-		this.listAllIndices()
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
+	constructor(
+		@Inject('MongoClient') private readonly mongoClient: MongoClient,
+		@Inject('ESClient') private readonly esClient: Client,
+	) {
+		this.init(path.join(__dirname, '../configs'));
+	}
+
+	async init(configDir: string) {
+		const configFiles = await fs.readdir(configDir);
+		for (const configFile of configFiles) {
+			const config = JSON.parse(await fs.readFile(configDir + '/' + configFile, 'utf8'));
+			await this.upsertIndex(config.index_params);
+		}
 	}
 
 	async listAllIndices() {
-		const response = await this.esClient.cat.indices({ format: 'json' });
-		return response;
+		return this.esClient.cat.indices({ format: 'json' });
+	}
+
+	async createIndex(params: IndicesCreateRequest) {
+		return this.esClient.indices.create(params);
+	}
+
+	async updateMapping(params: IndicesCreateRequest) {
+		const mappingRequest: IndicesPutMappingRequest = {
+			...params.mappings,
+			index: params.index,
+		};
+		await this.esClient.indices.putMapping(mappingRequest);
+	}
+
+	async upsertIndex(params: IndicesCreateRequest) {
+		try {
+			const exists = await this.esClient.indices.exists({
+				index: params.index,
+			});
+			console.log('exists', exists);
+			if (exists) {
+				console.log('index already exists, updating mapping');
+				return this.updateMapping(params);
+			}
+			console.log('index does not exist, creating');
+			return this.esClient.indices.create(params);
+		} catch (error) {
+			console.error('upsertIndex: error', error);
+		}
 	}
 }
